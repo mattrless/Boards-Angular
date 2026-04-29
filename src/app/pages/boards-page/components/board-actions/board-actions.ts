26a30116d716e72509e4dec7f8dc3360d4698a10
mattrless <carlosdanstd@gmail.com>
2026-04-29 03:01:49 +0000

    implement edit and delete board

--- src/app/pages/boards-page/components/board-actions/board-actions.ts
+++ src/app/pages/boards-page/components/board-actions/board-actions.ts
@@ -1,17 +1,43 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 import { HlmDropdownMenuImports } from '@spartan-ng/helm/dropdown-menu';
 import { HlmIcon } from '@spartan-ng/helm/icon';
-import { lucidePencil, lucideTrash, lucideEllipsis } from '@ng-icons/lucide';
+import { lucidePencil, lucideEllipsis, lucideTrash } from '@ng-icons/lucide';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmAlertDialogImports } from '@spartan-ng/helm/alert-dialog';
+import { BoardsStateService } from '../../../../services/boards-state.service';
+import { toast } from '@spartan-ng/brain/sonner';
+import { BoardsService } from '../../../../api/generated/boards/boards.service';
 
 @Component({
   selector: 'board-actions',
-  imports: [HlmDropdownMenuImports, HlmIcon, NgIcon, HlmButtonImports],
+  imports: [HlmDropdownMenuImports, HlmIcon, NgIcon, HlmButtonImports, HlmAlertDialogImports],
   providers: [provideIcons({ lucidePencil, lucideTrash, lucideEllipsis })],
   templateUrl: './board-actions.html',
 })
 export class BoardActions {
-  canUpdate = input.required<boolean>();
-  canDelete = input.required<boolean>();
+  private readonly boardsService = inject(BoardsService);
+  private readonly boardsStateService = inject(BoardsStateService);
+
+  readonly boardId = input.required<number>();
+
+  readonly canUpdate = input.required<boolean>();
+  readonly canDelete = input.required<boolean>();
+
+  readonly showForm = output<void>();
+
+  onCancel(ctx: { close: () => void }): void {
+    ctx.close();
+  }
+
+  onConfirmDelete(ctx: { close: () => void }) {
+    this.boardsService.removeBoard(this.boardId())
+    .subscribe({
+      next: () => {
+        toast.success("Board deleted");
+        this.boardsStateService.reload();
+        ctx.close();
+      }
+    });
+  }
 }
--- src/app/pages/boards-page/components/edit-board-form/edit-board-form.ts
+++ src/app/pages/boards-page/components/edit-board-form/edit-board-form.ts
@@ -1,11 +1,80 @@
-import { Component, input } from '@angular/core';
-import { BoardResponseDto } from '../../../../api/generated/model';
+import { Component, effect, inject, input, output, signal } from '@angular/core';
+import { BoardResponseDto, UpdateBoardDto } from '../../../../api/generated/model';
+import { HttpErrorResponse } from '@angular/common/http';
+import { NonNullableFormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { toast } from '@spartan-ng/brain/sonner';
+import { finalize } from 'rxjs';
+import { BoardsService } from '../../../../api/generated/boards/boards.service';
+import { BoardsStateService } from '../../../../services/boards-state.service';
+import { HlmButtonImports } from '@spartan-ng/helm/button';
+import { HlmFieldImports } from '@spartan-ng/helm/field';
+import { HlmInputImports } from '@spartan-ng/helm/input';
 
 @Component({
   selector: 'edit-board-form',
-  imports: [],
+  imports: [HlmFieldImports, HlmButtonImports, HlmInputImports, ReactiveFormsModule],
   templateUrl: './edit-board-form.html',
 })
 export class EditBoardForm {
   board = input.required<BoardResponseDto>();
+
+  private readonly fb = inject(NonNullableFormBuilder);
+  private readonly boardsService = inject(BoardsService);
+  private readonly boardsStateService = inject(BoardsStateService);
+
+  readonly closeBoardForm = output<void>();
+
+  readonly isSubmitting = signal(false);
+  readonly serverError = signal('');
+
+  readonly boardForm = this.fb.group({
+    name: ['', Validators.required]
+  });
+  // setting default value to input
+  constructor() {
+    effect(() => {
+      const board = this.board();
+      this.boardForm.patchValue({ name: board.name ?? '' });
+    });
+  }
+
+  onSubmit() {
+    if (this.isSubmitting()) return;
+
+    if(this.boardForm.invalid){
+      this.boardForm.markAllAsTouched();
+      return;
+    }
+
+    this.serverError.set('');
+    this.isSubmitting.set(true);
+
+    const boardFormValues: UpdateBoardDto = this.boardForm.getRawValue();
+
+    const boardId = this.board().id;
+    if (boardId == null) {
+      this.serverError.set('Invalid board id');
+      return;
+    }
+
+    this.boardsService.updateBoard(boardId, boardFormValues)
+    .pipe(finalize( () => {
+        this.isSubmitting.set(false);
+      }
+    ))
+    .subscribe({
+      next: () => {
+        toast.success("Board updated");
+        this.boardsStateService.reload();
+        this.closeBoardForm.emit();
+      },
+      error: (e: HttpErrorResponse) => {
+        if (e.status === 400) {
+          this.serverError.set('Invalid data');
+        } else {
+          this.serverError.set('Something went wrong, try again');
+        }
+      }
+    });
+  }
 }
